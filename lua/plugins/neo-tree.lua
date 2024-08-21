b2cccb9a76e69755edbcefd738ee3ab9430d0bd4
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2024-08-21 14:07:59 -0400

    cleaned up some commands and fixed LazyVim trying to load but I dont want it

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,8 +1,24 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      event_handlers = {
+
+        {
+          event = "file_open_requested",
+          handler = function()
+            -- auto close
+            vim.cmd("Neotree close")
+            -- OR
+            --require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+
+      }
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+  end,
 }
